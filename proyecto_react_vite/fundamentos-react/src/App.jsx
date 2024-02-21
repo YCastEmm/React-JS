b0b0559edbacd591258f8da00aef2e942cdb5d98
Yair <ycastagnola@x-28.com>
2024-02-21 18:41:18 -0300

    Agregado de JSX

--- proyecto_react_vite/fundamentos-react/src/App.jsx
+++ proyecto_react_vite/fundamentos-react/src/App.jsx
@@ -1,5 +1,14 @@
+/* El código JSX parece HTML pero no lo es, es en realidad JavaScript + XML*/
+
 let App = () =>{
     return <h1>Hola soy React.JS</h1>
+}
+
+export let seccion = ()=>{
+    return  <div>
+                <p>dos elementos de react dentro de un div</p>
+                <img src="https://picsum.photos/300/300" alt="" />
+            </div>
 }
 
 export default App
