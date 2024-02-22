797a0881316b0d973a0383c50c9845908f9967ec
Yair <ycastagnola@x-28.com>
2024-02-21 21:45:08 -0300

    Agregados componentes y fragments

--- proyecto_react_vite/fundamentos-react/src/App.jsx
+++ proyecto_react_vite/fundamentos-react/src/App.jsx
@@ -1,14 +1,48 @@
 /* El código JSX parece HTML pero no lo es, es en realidad JavaScript + XML*/
+/* Los nombres de los componentes deben comenzar con mayuscula */
+/* Los elementos debes estar contenido por otro elemento, no pueden estar flotando */
 
+import { Fragment } from "react"
+
+/* Conteniendo los elementos con un div*/
+/* Este componente se exporta por defecto abajo */
 let App = () =>{
-    return <h1>Hola soy React.JS</h1>
-}
-
-export let seccion = ()=>{
-    return  <div>
-                <p>dos elementos de react dentro de un div</p>
-                <img src="https://picsum.photos/300/300" alt="" />
+    return  <div className="div">
+                <h1 className="text-center">Hola soy React.JS</h1>
+                <p>tres elementos de react dentro de un div</p>
+                <img src="https://picsum.photos/100/100" alt="" />
             </div>
 }
+
+
+/* Usando un fragment para contener elementos (la forma más fácil) */
+/* En este componente se declara el export y luego en app.jsx se hace el import y se dice que componente se importa */
+export let Seccion = ()=>{
+    return  <>
+                <div>
+                    <p>Este fragment se hizo con &lt;&gt;&lt;/&gt;</p>
+                </div>
+                <img className="imagen" src="https://picsum.photos/100/100" alt="" />
+            </>
+}
+
+
+/* Usando un fragment para contener elementos*/
+/* En este componente se declara el export y luego en app.jsx se hace el import y se dice que componente se importa */
+export let FragmentImportado = ()=>{
+    return  <Fragment>
+                <div>
+                    <p>Este fragmente está hecho con &lt;React.Fragment&gt;&lt;/React.Fragment&gt; (previamente importado desde react</p>
+                </div>
+                <img className="imagen" src="https://picsum.photos/100/100" alt="" />
+            </Fragment>
+}
+
+
+
+
+
+
+
 
 export default App
--- proyecto_react_vite/fundamentos-react/src/main.jsx
+++ proyecto_react_vite/fundamentos-react/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {seccion} from "./App.jsx"
+import {Seccion, FragmentImportado} from "./App.jsx"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Seccion />
+    <FragmentImportado />
   </React.StrictMode>,
 )
