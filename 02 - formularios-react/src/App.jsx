dd652476faa95b77bc00cce3c3cc2e8953a04566
Yair <ycastagnola@x-28.com>
2024-02-27 18:11:13 -0300

    Estructura de carpetas de formulario no controlado

---
+++ 02 - formularios-react/src/App.jsx
@@ -0,0 +1,12 @@
+import { NoControlado } from "./components/no-controlado";
+
+let App = () => {
+    return (
+        <div className="container">
+            <h1>Formularios No Controlados</h1>
+            <NoControlado></NoControlado>
+        </div>
+    );
+};
+
+export default App;
---
+++ 02 - formularios-react/src/components/no-controlado.jsx
@@ -0,0 +1,1 @@
+export let NoControlado = () => { return "No controlado" }
