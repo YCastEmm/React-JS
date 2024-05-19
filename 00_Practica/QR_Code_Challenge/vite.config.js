228bb32b2c5db1ada1e522f4fb3d0cfe868f32bc
Yair Castagnola <ycastagnola>
2024-05-19 20:58:32 -0300

    base updated

--- 00_Practica/QR_Code_Challenge/vite.config.js
+++ 00_Practica/QR_Code_Challenge/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ycastemm.github.io/Frontend_Exercises/"
+  base: "https://YCastEmm.github.io/React-JS"
 })
