fa519262a0977c241ce94e48f1f54b898104001d
Yair Castagnola <ycastagnola>
2024-05-19 22:57:32 -0300

    Carpeta Build

--- 04 - toDo-App-Tailwind-Css/vite.config.js
+++ 04 - toDo-App-Tailwind-Css/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://YCastEmm.github.io/React-JS/builds/ToDo_App"
 })
