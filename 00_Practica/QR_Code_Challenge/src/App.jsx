026d9cdc109b323520ade66222abb4095870a269
Yair Castagnola <ycastagnola>
2024-05-19 21:28:10 -0300

    deploy

--- 00_Practica/QR_Code_Challenge/src/App.jsx
+++ 00_Practica/QR_Code_Challenge/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Card from "./components/Card";
 
 let title = "Improve your front-end skills by building projects"
-let texto = "Scan the QR code to visit Frontend Mentor and take your coiding skills to the next level."
+let texto = "Scan the QR code to visit Frontend Mentor and take your coiding skills to the next level"
 
 function App() {
 
